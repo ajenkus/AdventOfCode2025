97245799749c39002881b8798c7832b87a2550d0
Astijus Jenkus <astijus.jenkus@gmail.com>
2025-12-06 20:19:49 +0200

    Fix day 5 part 2

--- src/5/2.ts
+++ src/5/2.ts
@@ -1,42 +1,32 @@
 import * as fs from "fs"
 
 interface Range {
-    min: number
-    max: number
+    from: number
+    to: number
 }
 
 let ranges: Range[] = []
 let input: string[] = fs.readFileSync('src/5/input', 'utf8').split('\n')
-input.slice(0, input.findIndex(value => value == '\r')).forEach(line => {
-    const [min, max] = line.split('-').map(item => parseInt(item))
-    for (let i = 0; i < ranges.length; i++) {
-        if (min < ranges[i].max && max > ranges[i].max) {
-            ranges[i].max = max
-            ranges[i].min = Math.min(min, ranges[i].min)
-            return
-        }
-    }
-    ranges.push({ min, max })
+
+input.slice(0, input.findIndex(value => value == '')).forEach(line => {
+    const [from, to] = line.split('-').map(item => parseInt(item))
+    ranges.push({ from, to })
 })
+ranges.sort((a, b) => a.from - b.from)
 
-console.log(ranges.filter(item => {
-    let keep: boolean = true
-
-    for (let i = 0; i < ranges.length; i++) {
-        if (item == ranges[i]) { continue }
-
-        const [min, max] = Object.values(ranges[i])
-        if (item.min > min && item.max > max) {
-            keep = false
-        }
-
-        if (item.min > min && item.min < max) {
-            item.max = Math.max(max, item.max)
-            item.min = Math.min(min, item.min)
-            return true
+let removed: number
+do {
+    removed = 0
+    for (let i = 0; i < ranges.length - 1; i++) {
+        const a: Range = ranges[i]
+        const b: Range = ranges[i + 1]
+        if ((b.from >= a.from && b.from <= a.to) || (b.from <= a.from && b.to >= a.to)) {
+            a.from = Math.min(a.from, b.from)
+            a.to = Math.max(a.to, b.to)
+            ranges.splice(i + 1, 1)
+            removed++
         }
     }
-    return keep
-}).reduce((accumulator, item) => {
-    return accumulator + (item.max - item.min)
-}, 0))
+} while (removed > 0)
+
+console.log(ranges.reduce((count, range) => { return count + (range.to + 1 - range.from) }, 0))
